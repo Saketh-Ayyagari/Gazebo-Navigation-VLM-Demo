e29b48e13c2ab6b7c18a377a9161bf4059ec3834
dev-machine-inspiron-aniketh <sakethsarma07@gmail.com>
2026-07-24 14:01:54 -0400

    added additional warehouse world for testing loop closures

--- src/sakeths_first_robot_simulation/launch/launch_robot_sim.launch.py
+++ src/sakeths_first_robot_simulation/launch/launch_robot_sim.launch.py
@@ -29,7 +29,7 @@ def generate_launch_description():
     default_world = os.path.join(
         get_package_share_directory(package_name),
         'worlds',
-        'living_room.sdf' # change this if you want to change the world as long as it has necessary gazebo plugins imported. 
+        'tugbot_depot.sdf' # change this if you want to change the world as long as it has necessary gazebo plugins imported. 
     )    
     
     world = LaunchConfiguration('world')
