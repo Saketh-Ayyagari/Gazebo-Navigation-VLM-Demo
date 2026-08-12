debcc56297ee64c5d1fb4f264a13b9c562943d87
dev-machine-inspiron-aniketh <sakethsarma07@gmail.com>
2026-08-12 15:03:04 -0400

    added customizable framerate to image_subsciber node in case local machine has a hard time keeping up

--- src/sakeths_robot_vision/sakeths_robot_vision/node.py
+++ src/sakeths_robot_vision/sakeths_robot_vision/node.py
@@ -7,22 +7,38 @@ import cv2 as cv
 from ultralytics import YOLO
 import modules_to_import.opencv_utils as cv_utils
 
+FRAME_RATE = 10  # frames per second
+
 class ImageSubscriber(Node):
     def __init__(self):
         super().__init__('image_subscriber')
         self.subscription = self.create_subscription(
             Image,
             '/camera/image_raw',
-            self.listener_callback,
+            self.timer_callback,
             10)
         self.br = CvBridge()
         self.model = YOLO('yolov8n.pt')  # Load a pre-trained YOLOv8 model
+        
+        timer_period = 1 / FRAME_RATE # runs this node based on the frame rate (can be customized)
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
-    def listener_callback(self, data):
+    def timer_callback(self, msg):
+        self.camera_callback(msg)
+    '''
+    Runs YOLO model on specific frame and displays results. Converts ROS2 image message to OpenCV format 
+    using CvBridge, then runs YOLO model on the frame and displays the results using OpenCV.
+
+    @params
+    img: ROS2 image message.
+    '''
+    def camera_callback(self, img):
+        current_frame = self.br.imgmsg_to_cv2(img, desired_encoding="passthrough") # use "passthrough" to get the raw depth image, and "rgb8" for color images
+
         # self.get_logger().info('Receiving video frame')
         # As pointed in comments below modify the following to use bgr encoding
-        # current_frame = self.br.imgmsg_to_cv2(data)
-        current_frame = self.br.imgmsg_to_cv2(data, desired_encoding="passthrough") # use "passthrough" to get the raw depth image, and "rgb8" for color images
+        # current_frame = self.br.imgmsg_to_cv2(data, desired_encoding='bgr8')
+        
         results = self.model(current_frame)
         cv_utils.show_image(results[0].plot())
     
