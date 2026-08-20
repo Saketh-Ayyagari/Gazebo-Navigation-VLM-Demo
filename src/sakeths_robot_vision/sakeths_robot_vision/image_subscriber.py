5eeedb5398b3e7f96a4b9b6f20e1bcf435496cc5
dev-machine-inspiron-aniketh <sakethsarma07@gmail.com>
2026-08-20 02:24:18 -0400

    changed custom message name to CustomMessage (instead of CustomMsg). Created another node that takes messages from rgb, depth, and odom topics to put into one message type before processing in the instance segmentation pipeline

--- src/sakeths_robot_vision/sakeths_robot_vision/image_subscriber.py
+++ src/sakeths_robot_vision/sakeths_robot_vision/image_subscriber.py
@@ -7,36 +7,54 @@ import cv2 as cv
 from ultralytics import YOLO
 import modules_to_import.opencv_utils as cv_utils
 
-FRAME_RATE = 10  # frames per second
 
 class ImageSubscriber(Node):
     def __init__(self):
         super().__init__('image_subscriber')
-        self.subscription = self.create_subscription(
-            Image,
-            '/camera/image_raw',
-            self.camera_callback,
-            10)
         self.br = CvBridge()
         self.model = YOLO('yolov8n.pt')  # Load a pre-trained YOLOv8 model
-        
+
+        self.subscription = self.create_subscription(
+            Image,
+            '/camera/depth_image',
+            self.camera_callback,
+            10)
+                
 
     '''
-    Runs YOLO model on specific frame and displays results. Converts ROS2 image message to OpenCV format 
-    using CvBridge, then runs YOLO model on the frame and displays the results using OpenCV.
+    Runs pipeline for object detection
+
+    1. Given RGB-D images and odometry data, first segments RGB image into different objects using YOLOv8
+    (can be replaced with a VLM/other image segmentation model of some sort). Uses the bounding boxes to
+    get the approximate center of the object in the RGB image. 
+    2. Uses depth image to get the distance to the object in the RGB image. 
+    3. Uses pinhole camera model to get 3D position of any object relative to the camera.
+    4. Combines the 3D position of the object with the odometry data to get the position of the object 
+    in the world frame.
+    5. Stores results in JSON file. Results include
+        - Object category.
+        - Estimated position of the object in world frame.
 
     @params
-    img: ROS2 image message.
+
+    RGB Image for image segmentation.
+    Depth image for calculating distances to objects in RGB image.
+    Odometry message containing robot's current pose.
     '''
     def camera_callback(self, img):
-        current_frame = self.br.imgmsg_to_cv2(img, desired_encoding="passthrough") # use "passthrough" to get the raw depth image, and "rgb8" for color images
+        depth_frame = self.br.imgmsg_to_cv2(img, desired_encoding="passthrough") # use "passthrough" to get the raw depth image, and "rgb8" for color images
 
         # self.get_logger().info('Receiving video frame')
         # As pointed in comments below modify the following to use bgr encoding
         # current_frame = self.br.imgmsg_to_cv2(data, desired_encoding='bgr8')
 
-        results = self.model(current_frame)
+        results = self.model(current_frame) # running YOLO model for instance segmentation
         cv_utils.show_image(results[0].plot())
+
+        # given bounding box coordinates, calculate center of the bounding box and use it to get the depth value from the depth image.
+
+        # storing results in a JSON file. Results consist of instance of a category and the estimated
+        # position of the object. The estimated position is calculated using the depth image and the.
     
 def main(args=None):
     rclpy.init(args=args)
